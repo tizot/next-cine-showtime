dd07eadefba845f6d03f06590eeef7a232c3ea10
Camille Masset <masset.camille@gmail.com>
2022-12-17 11:36:47 +0100

    More automation in deployment script

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -1,5 +1,8 @@
 #!/usr/bin/env zx
 
+// Just to remember 1Password password / TouchID
+await $`op item list`.quiet();
+
 if ((await $`git status --porcelain`).stdout) {
   console.error(
     chalk.yellow(
@@ -20,4 +23,18 @@ await $`zx scripts/clean.mjs`;
 await $`docker build -t tizot/cine:${nextVersion} -t tizot/cine:latest .`;
 await $`docker image push tizot/cine:${nextVersion}`;
 await $`docker image push tizot/cine:latest`;
-await $`ssh vps -t 'sudo bash cine-refresh.sh'`;
+
+// From now on, sensitive information can be displayed in commands
+const initialLog = $.log;
+$.log = (entry) => {
+  if (entry.kind === "cmd") return;
+  initialLog(entry);
+};
+
+const pwd = JSON.parse(
+  (
+    await $`op item get 3asebhcozze6vnpdd65aopt4wy --fields label=password --format json`.quiet()
+  ).stdout
+).value;
+$.log("Deploying to VPS");
+await $`echo ${pwd} | ssh vps -t 'sudo -S bash cine-refresh.sh'`;
