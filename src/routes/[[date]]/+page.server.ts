5fcc45fa566d3fba032adc0f187ae3c554df617e
Camille Masset <camille@masset.email>
2023-10-01 15:17:28 +0200

    chore: use spaces and not tabs

--- src/routes/[[date]]/+page.server.ts
+++ src/routes/[[date]]/+page.server.ts
@@ -1,5 +1,5 @@
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = ({ params }) => {
-	return { date: params.date ? new Date(params.date) : new Date() };
+  return { date: params.date ? new Date(params.date) : new Date() };
 };
