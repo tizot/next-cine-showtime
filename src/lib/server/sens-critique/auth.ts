b313617820f75a8fc95ba49f4457a139b05e01fb
Camille Masset <camille@masset.email>
2023-10-01 16:41:05 +0200

    feature: add function to retrieve ratings from sens critique

---
+++ src/lib/server/sens-critique/auth.ts
@@ -0,0 +1,65 @@
+import { isAfter } from 'date-fns';
+import { SENS_CRITIQUE_PASSWORD } from '$env/static/private';
+
+const FIREBASE_REST_API = 'https://identitytoolkit.googleapis.com/v1/accounts';
+const API_KEY = 'AIzaSyAHxGE6otUcjogt6EXNzXrAZJr99WZ1MdI';
+
+class SensCritiqueAuth {
+  private _token: string | null;
+
+  constructor(private email: string, private password: string) {
+    this._token = null;
+  }
+
+  async getToken() {
+    await this._refresh();
+    if (this._token == null) throw new Error('Failed to refresh SensCritique token');
+    return this._token;
+  }
+
+  refresh() {
+    this._refresh();
+  }
+
+  async _refresh(force = false) {
+    if (!force && !this._has_token_expired()) return;
+
+    console.log('Refreshing SC token');
+    const url = `${FIREBASE_REST_API}:signInWithPassword?key=${API_KEY}`;
+    const headers = { 'content-type': 'application/json; charset=UTF-8' };
+    const body = JSON.stringify({
+      email: this.email,
+      password: this.password,
+      returnSecureToken: true,
+    });
+    const response = await fetch(url, {
+      method: 'POST',
+      headers,
+      body,
+    });
+    if (response.status !== 200) {
+      throw new Error(await response.text());
+    }
+    const data = await response.json();
+    this._token = data.idToken;
+  }
+
+  _has_token_expired() {
+    if (this._token === null) return true;
+    const claims = JSON.parse(
+      Buffer.from(this._token.split('.')[1] + '==', 'base64').toString('ascii'),
+    );
+    const expirationSeconds = claims['exp'];
+    if (expirationSeconds) {
+      if (isAfter(expirationSeconds * 1000, new Date())) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+export default new SensCritiqueAuth(
+  'masset.camille_senscritique@gmail.com',
+  SENS_CRITIQUE_PASSWORD,
+);
---
+++ src/lib/server/sens-critique/index.ts
@@ -0,0 +1,67 @@
+import { GraphQLClient, gql } from 'graphql-request';
+import { deburr } from 'lodash';
+import auth from './auth';
+import { cache } from '../cache';
+
+type GqlSensCritiqueRating = {
+  searchResult: {
+    results: Array<{
+      universe: string;
+      products_list: Array<{
+        title: string;
+        year_of_production: number;
+        rating: string;
+        url: string;
+      }>;
+    }>;
+  };
+};
+
+const GET_SENSCRITIQUE_RATING = gql`
+  query Search($title: String!) {
+    searchResult(keywords: $title, limit: 15, universe: "movie") {
+      results {
+        universe
+        products_list {
+          title
+          year_of_production
+          rating
+          url
+        }
+      }
+    }
+  }
+`;
+
+const _clean = (s: string) => {
+  return deburr(s.toLocaleLowerCase())
+    .replace(/[^a-z0-9]/g, '')
+    .replace(/\s/g, '');
+};
+
+export async function _search(title: string, year: number) {
+  const token = await auth.getToken();
+  const client = new GraphQLClient('https://gql.senscritique.com/graphql', {
+    headers: { Authorization: token },
+  });
+  const data: GqlSensCritiqueRating = await client.request(GET_SENSCRITIQUE_RATING, { title });
+  const movies = data.searchResult.results.find((p) => p.universe == 'movie') ?? {
+    products_list: [],
+  };
+  const match = movies.products_list.find(
+    // Sometimes, SensCritique says the movie is from 2021 but Allocine says it is from 2022.
+    // Since it is highly unlikely that two different movies with the same name are produced
+    // less than 2 years apart, this condition is enough.
+    (m) => Math.abs(m.year_of_production - year) <= 2 && _clean(m.title) === _clean(title),
+  );
+  if (!match) {
+    return { rating: null, url: null };
+  }
+  const rating = parseFloat(match['rating']);
+  if (isNaN(rating)) {
+    return { rating: null, url: null };
+  }
+  return { rating, url: match['url'] };
+}
+
+export const search = cache(_search);
