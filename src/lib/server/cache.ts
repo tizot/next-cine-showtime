8f04bd026477d87b4c077929a5c8f6142c438b2f
Camille Masset <camille@masset.email>
2023-10-01 16:30:55 +0200

    feature: implement cache

---
+++ src/lib/server/cache.ts
@@ -0,0 +1,26 @@
+import { add, hoursToMilliseconds, isBefore } from 'date-fns';
+
+export const ONE_HOUR = hoursToMilliseconds(1);
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const cache = <F extends (...args: any[]) => any>(fn: F, ttlMilliseconds = ONE_HOUR) => {
+  let store: Record<string, [Awaited<ReturnType<F>>, Date]> = {};
+  const memoized = async (...args: Parameters<F>): Promise<Awaited<ReturnType<F>>> => {
+    const key = JSON.stringify(args);
+    const now = new Date();
+    if (key in store) {
+      const [result, eviction] = store[key];
+      if (isBefore(now, eviction)) {
+        return result;
+      }
+    }
+    const result = await fn(...args);
+    store[key] = [result, add(now, { seconds: ttlMilliseconds / 1000 })];
+    return result;
+  };
+  memoized.evictAll = () => {
+    store = {};
+  };
+
+  return memoized;
+};
