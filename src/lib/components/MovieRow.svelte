a35cb119dadca3d300d9d3032d06269023af7a1c
Camille Masset <camille@masset.email>
2023-10-02 09:37:16 +0200

    style: fix white-space wrapping and reduce font size

--- src/lib/components/MovieRow.svelte
+++ src/lib/components/MovieRow.svelte
@@ -71,7 +71,7 @@
   }
 
   tr {
-    text-wrap: nowrap;
+    white-space: nowrap;
   }
 
   td > a {
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<slot />
+
+<style>
+  @media only screen {
+    :root {
+      --font-size: calc(var(--font-size) * 0.8);
+    }
+  }
+</style>
