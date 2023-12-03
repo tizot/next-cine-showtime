9b0115d661d6dbed9eece2382c78586f22ac912f
Camille Masset <camille@masset.email>
2023-12-03 19:47:08 +0100

    feature: add sorting capabilities

--- src/lib/components/Showtimes.svelte
+++ src/lib/components/Showtimes.svelte
@@ -1,32 +1,95 @@
 <script lang="ts">
-  import { size } from 'lodash-es';
+  import { deburr, orderBy, size } from 'lodash-es';
   import type { Movie } from '$lib/types';
   import MovieRow from './MovieRow.svelte';
+  import Fa from 'svelte-fa';
+  import { faSortUp, faSortDown } from '@fortawesome/free-solid-svg-icons';
 
   export let movies: Movie[] = [];
 
+  type Column = {
+    header: string;
+    sorter?: keyof Movie | ((movie: Movie) => string) | ((movie: Movie) => number);
+    reverseByDefault?: boolean;
+  };
+  const columns = [
+    'title',
+    'duration',
+    'sensCritiqueRating',
+    'userRating',
+    'pressRating',
+    'cinema',
+    'showtimes',
+  ] as const;
+  type ColumnName = typeof columns[number];
+
+  const columnDefs: Record<ColumnName, Column> = {
+    title: { header: 'Titre', sorter: ({ title }) => deburr(title.toLowerCase()) },
+    duration: {
+      header: 'Durée',
+      sorter: 'duration',
+      reverseByDefault: true,
+    },
+    sensCritiqueRating: {
+      header: 'Sens Critique',
+      sorter: 'sensCritiqueRating',
+      reverseByDefault: true,
+    },
+    userRating: {
+      header: 'Spectateurs',
+      sorter: 'userRating',
+      reverseByDefault: true,
+    },
+    pressRating: {
+      header: 'Presse',
+      sorter: 'pressRating',
+      reverseByDefault: true,
+    },
+    cinema: { header: 'Cinéma' },
+    showtimes: { header: 'Horaires' },
+  };
+
+  let sortBy: ColumnName | null = 'sensCritiqueRating';
+  let sortDirection: 'asc' | 'desc' = 'desc';
+
+  $: sortByColumn = sortBy == null ? null : columnDefs[sortBy];
+
+  function handleOnColumnClick(column: ColumnName) {
+    if (sortBy === column) {
+      sortDirection = sortDirection === 'asc' ? 'desc' : 'asc';
+    } else if (columnDefs[column].sorter != null) {
+      sortBy = column;
+      sortDirection = columnDefs[column].reverseByDefault ? 'desc' : 'asc';
+    }
+  }
+
   $: movies_ = movies.map((m) => ({ ...m, _theatersCount: size(m.showtimes) }));
+  $: sortedMovies =
+    sortByColumn == null || sortByColumn.sorter == null
+      ? movies_
+      : orderBy(movies_, [sortByColumn.sorter], [sortDirection]);
 </script>
 
 <figure>
   <table>
     <thead>
       <tr>
-        <th scope="col">Titre</th>
-        <th scope="col">Durée</th>
-        <th scope="col">Sens Critique</th>
-        <th scope="col">Spectateurs</th>
-        <th scope="col">Presse</th>
-        <th scope="col">Cinéma</th>
-        <th scope="col">Horaires</th>
+        {#each columns as col}
+          <th scope="col" on:click={() => handleOnColumnClick(col)}>
+            {columnDefs[col].header}
+            {#if sortBy === col}
+              <Fa icon={sortDirection === 'asc' ? faSortUp : faSortDown} />
+            {/if}
+          </th>
+        {/each}
       </tr>
     </thead>
-    {#if movies_.length === 0}
+    {#if sortedMovies.length === 0}
       <tbody>
         <tr><td colspan="7">Aucun film...</td></tr>
       </tbody>
     {:else}
-      {#each movies_ as movie} <MovieRow {movie} />{/each}
+      {#each sortedMovies as movie} <MovieRow {movie} />{/each}
     {/if}
   </table>
 </figure>
