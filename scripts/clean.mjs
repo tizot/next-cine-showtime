69c982d1a7f7dc0e7129e9e5cc6b09385d00ae2d
Camille Masset <camille@masset.email>
2024-08-10 17:32:34 +0200

    chore: remove unused files

--- scripts/clean.mjs
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zx
-
-$.verbose = false;
-
-const root = (await $`git rev-parse --show-toplevel`).stdout.trim();
-
-cd(root);
-await $`rm -rf node_modules`;
-await $`pnpm run clean:all`;
