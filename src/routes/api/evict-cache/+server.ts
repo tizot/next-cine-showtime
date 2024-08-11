dbacb09fa8cfe20dec621da1082d9155eaf9bd5e
Camille Masset <camille@masset.email>
2024-08-11 10:34:45 +0200

    feat: configure CronJob with Vercel

--- src/routes/api/evict-cache/+server.ts
+++ src/routes/api/evict-cache/+server.ts
@@ -1,11 +1,14 @@
-import type { RequestHandler } from '@sveltejs/kit';
+import { error, type RequestHandler } from '@sveltejs/kit';
 import { clearCache } from '$lib/server/cache';
 import { MOVIES_KV_PREFIX, SENS_CRITIQUE_KV_PREFIX } from '$lib/server/constants';
+import { CRON_SECRET } from '$env/static/private';
 
-export const GET: RequestHandler = async () => {
+export const GET: RequestHandler = async ({ request }) => {
+  if (request.headers.get('Authorization') !== `Bearer ${CRON_SECRET}`) {
+    error(401, 'Unauthorized');
+  }
+
   console.log('Daily cache eviction triggered');
-
   await Promise.all([clearCache(MOVIES_KV_PREFIX), clearCache(SENS_CRITIQUE_KV_PREFIX)]);
-
-  return new Response('Cache cleared', { status: 200 });
+  return new Response('Cache evicted', { status: 200 });
 };
