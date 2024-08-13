a585ed7b252db0946c0f944b3bb006acfc0e727d
Camille Masset <camille@masset.email>
2024-08-13 10:42:00 +0200

    chore: trigger build

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [sentrySvelteKit({
-    sourceMapsUploadOptions: {
-      org: "camille-masset",
-      project: "next-cine-showtime"
-    }
-  }), sveltekit()],
+  plugins: [
+    sentrySvelteKit({
+      sourceMapsUploadOptions: {
+        org: 'camille-masset',
+        project: 'next-cine-showtime',
+      },
+    }),
+    sveltekit(),
+  ],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
   },
 });
+
